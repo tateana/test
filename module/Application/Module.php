24b3780709482d5530e8ebac68c7a8e279fb4e13
tatiana <tatianabelichenko@gmail.com>
2015-04-29 18:07:11 +0200

    Add logging of errors

--- module/Application/Module.php
+++ module/Application/Module.php
@@ -11,16 +11,59 @@ namespace Application;
 
 use Zend\Mvc\ModuleRouteListener;
 use Zend\Mvc\MvcEvent;
+use Zend\ModuleManager\Feature;
+use Zend\EventManager\EventInterface;
 
-class Module
+class Module implements Feature\ConfigProviderInterface,
+                        Feature\AutoloaderProviderInterface,
+                        Feature\BootstrapListenerInterface
 {
-    public function onBootstrap(MvcEvent $e)
+    public function onBootstrap(EventInterface $event)
     {
-        $eventManager        = $e->getApplication()->getEventManager();
+    	$app = $event->getApplication();
+        $eventManager        = $app->getEventManager();
         $moduleRouteListener = new ModuleRouteListener();
         $moduleRouteListener->attach($eventManager);
+        
+        $logApp = $app->getServiceManager()->get('Log\App');
+        register_shutdown_function(array($this, 'onShutdown'), $logApp);
+        
+        $eventManager->attach(\Zend\Mvc\MvcEvent::EVENT_DISPATCH_ERROR, array($this, 'onError'));
+        $eventManager->attach(\Zend\Mvc\MvcEvent::EVENT_RENDER_ERROR, array($this, 'onError'));
     }
 
+    /**
+     * @param Zend\Log\Logger $logger
+     */
+    public function onShutdown($logger) {
+    	$error = error_get_last();
+
+    	if($error) {
+    		$logger->emerg($error['message'], $error);
+    	}
+    }
+    
+    /**
+    * @param Zend\Mvc\MvcEvent $e
+    */
+    public function onError($e)
+    {
+    	$sm = $e->getApplication()->getServiceManager();
+    	$logger = $sm->get('Log\App');
+    	if ($e->getParam('exception')){
+    		$exception = $e->getParam('exception');
+    		$logger->crit($exception->getMessage(), array($exception));
+    		return;
+    	}
+    
+    	if($e->getError() === \Zend\Mvc\Application::ERROR_ROUTER_NO_MATCH) {
+    		$logger->notice('Response Code 404 '.$e->getRequest()->getUriString());
+    		return;
+    	}
+    
+    	$logger->crit('Not defined exception '.$e->getName().' '.$e->getError(), $e->getParams());
+    }
+    
     public function getConfig()
     {
         return include __DIR__ . '/config/module.config.php';
